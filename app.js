5849be907d5ba0d8fb21026baa0606056d5b8442
jayanth <jayanthkumar598@gmail.com>
2023-12-15 21:18:41 +0530

    deploy

---
+++ app.js
@@ -0,0 +1,99 @@
+let boxes=document.querySelectorAll(".box");
+let reset=document.querySelector("#reset-button");
+
+let newGame=document.querySelector("#new-button");
+
+let msgCont=document.querySelector(".msg-container");
+
+let msg=document.querySelector("#msg");
+
+let turnO=true;
+
+
+let winning=[[0,1,2],[0,3,6],[0,4,8],
+             [1,4,7],[2,5,8],[2,4,6],[3,4,5],
+             [6,7,8],
+];
+
+
+boxes.forEach((box)=>
+{
+box.addEventListener("click",()=>
+{
+  if(turnO)
+  {
+    box.innerText="O";
+    turnO=false;
+    }
+    else
+    {
+      box.innerText="X";
+      turnO=true;
+    }
+
+    box.disabled=true;
+
+checkWinner();
+
+});
+});
+const resetGame=()=>
+{
+  turnO=true;
+  
+  enableBoxes();
+
+  
+  msgCont.classList.add("hide");
+
+}
+
+const disableBoxes=()=>
+{
+  for(let box of boxes)
+  {
+    box.disabled=true;
+  }
+}
+const enableBoxes=()=>
+{
+  
+  for(let box of boxes)
+  {
+    box.disabled=false;
+    box.innerText="";
+  }
+}
+const showWinner=(winner)=>
+{
+  msg.innerText=`CONGRATULATIONS WINNER IS ${winner}`;
+  
+  msgCont.classList.remove("hide");
+  disableBoxes();
+}
+const checkWinner=()=>{
+  for(let pattern of winning)
+  {
+  let pos1=boxes[pattern[0]].innerText;
+  let pos2=boxes[pattern[1]].innerText;
+  let pos3=boxes[pattern[2]].innerText;
+  
+  if(pos1!="" && pos2!="" && pos3!="")
+  {
+    if(pos1 === pos2  && pos2===pos3)
+    {
+
+      showWinner(pos1);
+      
+    }
+  }
+
+  }
+}
+
+
+
+
+reset.addEventListener("click",resetGame);
+newGame.addEventListener("click",resetGame);
+
